f2f504f3fa310ed7a7d69754357cfd838361c75c
Joao Lino <linusmannuel@gmail.com>
2023-05-03 17:25:05 +0100

    capturando dados da tela

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#novoItem');
+form.addEventListener('submit', (evento) => {
+	evento.preventDefault();
+	console.log(evento);
+	console.log(evento.target.elements['nome'].value);
+	console.log(evento.target.elements['quantidade'].value);
+});
