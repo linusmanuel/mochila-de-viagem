32fcfd84fc8ff916a323a34e480fc5b06f2cb342
Joao Lino <linusmannuel@gmail.com>
2023-05-03 18:04:03 +0100

    criando as validacoes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 const form = document.querySelector('#novoItem');
+const lista = document.querySelector('.lista');
+
 form.addEventListener('submit', (evento) => {
 	evento.preventDefault();
-	console.log(evento);
-	console.log(evento.target.elements['nome'].value);
-	console.log(evento.target.elements['quantidade'].value);
+	console.log();
+	creaElemento(
+		evento.target.elements['nome'].value,
+		evento.target.elements['quantidade'].value
+	);
 });
+
+const creaElemento = (nome, quantidade) => {
+	const novoItem = document.createElement('li');
+	novoItem.classList.add('item');
+
+	const numeroItem = document.createElement('strong');
+	numeroItem.innerHTML = quantidade;
+
+	novoItem.appendChild(numeroItem);
+	novoItem.innerHTML += nome;
+
+	lista.appendChild(novoItem);
+};
