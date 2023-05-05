1ceaaa3608b4f09d2610a910ed8e5052ee36d73a
Joao Lino <linusmannuel@gmail.com>
2023-05-05 23:08:26 +0100

    Modificar um item do localStorage

--- js/main.js
+++ js/main.js
@@ -2,17 +2,32 @@ const form = document.querySelector('#novoItem');
 const lista = document.querySelector('.lista');
 const items = JSON.parse(localStorage.getItem('items')) || [];
 
+items.forEach((el) => {
+	creaElemento(el);
+});
+
 form.addEventListener('submit', (evento) => {
 	evento.preventDefault();
 	const nome = evento.target.elements['nome'];
 	const quantidade = evento.target.elements['quantidade'];
+
+	const existe = items.find((elemento) => elemento.nome === nome.value);
 
 	const itemAtual = {
 		'nome': nome.value,
 		'quantidade': quantidade.value,
 	};
 
-	creaElemento(itemAtual);
+	if (existe) {
+		itemAtual.id = existe.id;
+		console.log(existe.id);
+		atualizaElemento(itemAtual);
+	} else {
+		itemAtual.id = items.length;
+		creaElemento(itemAtual);
+
+		items.push(itemAtual);
+	}
 
 	items.push(itemAtual);
 
@@ -22,19 +37,19 @@ form.addEventListener('submit', (evento) => {
 	quantidade.value = '';
 });
 
-const creaElemento = (item) => {
+function creaElemento(item) {
 	const novoItem = document.createElement('li');
 	novoItem.classList.add('item');
 
 	const numeroItem = document.createElement('strong');
 	numeroItem.innerHTML = item.quantidade;
-
+	numeroItem.dataset.id = item.id;
 	novoItem.appendChild(numeroItem);
 	novoItem.innerHTML += item.nome;
 
 	lista.appendChild(novoItem);
-};
+}
 
-items.forEach((el) => {
-	creaElemento(el);
-});
+function atualizaElemento(item) {
+	document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade;
+}
